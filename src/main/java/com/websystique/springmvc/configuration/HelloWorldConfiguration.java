b37d9c976dbd77b5f9cc1967a6673521938385b5
Meirzhan Rymbayev <meirzhan_rumbaev@mail.ru>
2016-03-13 12:22:59 +0600

    [IMP] Initialization Class and Configuration class was created

---
+++ src/main/java/com/websystique/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,11 @@
+package com.websystique.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.websystique.springmvc")
+public class HelloWorldConfiguration {
+}
