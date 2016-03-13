568122faf163c366c7f9c3cd3f557be5c2ac9c30
Meirzhan Rymbayev <meirzhan_rumbaev@mail.ru>
2016-03-13 11:08:44 +0600

    [INI] Initial structure was created

--- src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
+++ src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
@@ -8,9 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-/**
- * Created by Meirzhan_Rymbayev on 3/4/2016.
- */
 @RestController
 public class HelloWorldRestController {
 
